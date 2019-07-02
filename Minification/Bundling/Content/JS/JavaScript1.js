36ee1b91bf1d815b58a2009c35b540addc5d5a3a
AbubakkarMustafa <38445840+AbubakkarMustafa@users.noreply.github.com>
2019-07-02 08:50:59 +0500

    Minification uploading

---
+++ Minification/Bundling/Content/JS/JavaScript1.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    for (var i = 0; i < 5; i++) {
+
+
+
+        //variable decalration
+        var thatisnewvariable = i;
+
+
+        //message shown
+        console.log("hello from js 1 and i =" + thatisnewvariable);
+    }
+    
+})
